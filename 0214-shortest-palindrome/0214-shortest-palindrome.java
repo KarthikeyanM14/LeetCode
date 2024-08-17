af1e001c54c8ccefffe859e06b953d7168ab0ac9
karthikeyan <138330810+KarthikeyanM14@users.noreply.github.com>
2024-08-17 15:41:18 +0530

    Time: 280 ms (22.01%), Space: 45.8 MB (5.22%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.java
+++ 0214-shortest-palindrome/0214-shortest-palindrome.java
@@ -1,11 +1,23 @@
 class Solution {
-  public String shortestPalindrome(String s) {
-    final String t = new StringBuilder(s).reverse().toString();
-
-    for (int i = 0; i < t.length(); ++i)
-      if (s.startsWith(t.substring(i)))
-        return t.substring(0, i) + s;
-
-    return t + s;
-  }
+    public String shortestPalindrome(String s) {
+        int n=s.length();
+        char[] ch=s.toCharArray(); 
+        String str=""; 
+        for (int i=n-1;i>=0;i--) {  
+            boolean isPalindrome=true;  
+            for (int j=0;j<=i;j++) { 
+                if (ch[j]!=ch[i-j]){
+                    isPalindrome=false;  
+                    break; 
+                }
+            }
+            if(!isPalindrome){
+                str=str+ch[i]; 
+            }else{
+                break; 
+            }
+        }
+        String a=str+s;
+        return a;
+    }  
 }
