bfa5a72c798bc80ca0b8824dd649c267d0f0739b
karthikeyan <138330810+KarthikeyanM14@users.noreply.github.com>
2024-08-18 14:37:53 +0530

    Time: 2 ms (86.05%), Space: 55.6 MB (38.26%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -9,7 +9,7 @@ class Solution {
             product=product*nums[i];
         }
         product=1;
-        for(int i=n-1;i>=0;i--)
+        for(int i=n-1;i>=-0;i--)
         {
             arr[i]=product*arr[i];
             product=product*nums[i];
